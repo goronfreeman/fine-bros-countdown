ebe565b0f75d82e043350b36473efd4e9eccd700
Hunter Braun <hunter.braun@gmail.com>
2016-02-05 23:45:02 -0600

    Add analytics

--- dist/js/all.js
+++ dist/js/all.js
@@ -1,3 +1,18 @@
+  (function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function() {
+      (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+      m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+  })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+  ga('create', 'UA-62559923-2', 'auto');
+  ga('send', 'pageview');
+
 // Modified from source: http://stackoverflow.com/a/18078705/772086
 var ajax = {};
 ajax.x = function() {
